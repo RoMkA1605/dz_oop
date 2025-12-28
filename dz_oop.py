7864b6e3011272bf6245ebc049e8894346eed8f7
Roman_Sidorevich <romka1605@mail.ru>
2025-12-28 17:16:54 +0300

    Task_3 commit

--- dz_oop.py
+++ dz_oop.py
@@ -13,6 +13,18 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        grades = [g for course in self.grades.values() for g in course]
+        return sum(grades) / len(grades) if grades else 0
+
+    def __str__(self):
+        return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
+                f'Средняя оценка за ДЗ: {self.average_grade():.1f}\n'
+                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+                f'Завершенные курсы: {", ".join(self.finished_courses)}')
+
+    def __lt__(self, other):
+        return self.average_grade() < other.average_grade()
 
 class Mentor:
     def __init__(self, name, surname):
@@ -25,6 +37,17 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def average_grade(self):
+        grades = [g for course in self.grades.values() for g in course]
+        return sum(grades) / len(grades) if grades else 0
+
+    def __str__(self):
+        return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.average_grade():.1f}')
+
+    def __lt__(self, other):
+        return self.average_grade() < other.average_grade()
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
@@ -35,24 +58,28 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
 # lecturer = Lecturer('Иван', 'Иванов')
 # reviewer = Reviewer('Пётр', 'Петров')
 # print(isinstance(lecturer, Mentor)) # True
 # print(isinstance(reviewer, Mentor)) # True
 # print(lecturer.courses_attached)    # []
 # print(reviewer.courses_attached)    # []
+#
+# lecturer = Lecturer('Иван', 'Иванов')
+# reviewer = Reviewer('Пётр', 'Петров')
+# student = Student('Алёхина', 'Ольга', 'Ж')
+#
+# student.courses_in_progress += ['Python', 'Java']
+# lecturer.courses_attached += ['Python', 'C++']
+# reviewer.courses_attached += ['Python', 'C++']
+#
+# print(student.rate_lecturer(lecturer, 'Python', 7))  # None
+# print(student.rate_lecturer(lecturer, 'Java', 8))  # Ошибка
+# print(student.rate_lecturer(lecturer, 'С++', 8))  # Ошибка
+# print(student.rate_lecturer(reviewer, 'Python', 6))  # Ошибка
+#
+# print(lecturer.grades)  # {'Python': [7]}
 
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
-
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
-
-print(student.rate_lecturer(lecturer, 'Python', 7))  # None
-print(student.rate_lecturer(lecturer, 'Java', 8))  # Ошибка
-print(student.rate_lecturer(lecturer, 'С++', 8))  # Ошибка
-print(student.rate_lecturer(reviewer, 'Python', 6))  # Ошибка
-
-print(lecturer.grades)  # {'Python': [7]}
