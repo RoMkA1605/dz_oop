434d22249bc24fa31158b45d984f821106f914c7
Roman_Sidorevich <romka1605@mail.ru>
2025-12-28 16:53:48 +0300

    Third commit

--- dz_oop.py
+++ dz_oop.py
@@ -7,6 +7,12 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            lecturer.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -23,9 +29,30 @@ class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            student.grades.setdefault(course, []).append(grade)
+        else:
+            return 'Ошибка'
+
+# lecturer = Lecturer('Иван', 'Иванов')
+# reviewer = Reviewer('Пётр', 'Петров')
+# print(isinstance(lecturer, Mentor)) # True
+# print(isinstance(reviewer, Mentor)) # True
+# print(lecturer.courses_attached)    # []
+# print(reviewer.courses_attached)    # []
+
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
-print(isinstance(lecturer, Mentor)) # True
-print(isinstance(reviewer, Mentor)) # True
-print(lecturer.courses_attached)    # []
-print(reviewer.courses_attached)    # []
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecturer(lecturer, 'Python', 7))  # None
+print(student.rate_lecturer(lecturer, 'Java', 8))  # Ошибка
+print(student.rate_lecturer(lecturer, 'С++', 8))  # Ошибка
+print(student.rate_lecturer(reviewer, 'Python', 6))  # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
